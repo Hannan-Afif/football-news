c811e447cd843b9b551a41a36f5d563106cd6994
Hannan-Afif <hannanafifd@gmail.com>
2025-09-10 09:13:46 +0700

    Revisi

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
+        'npm' : '2406350394',
+        'name': 'Hannan Afif Darmawan',
         'class': 'PBP A'
     }
 
